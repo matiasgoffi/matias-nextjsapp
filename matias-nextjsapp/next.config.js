c0d3debb8a65c02b3fd8b1860675e1b08a2e9aa7
matiasgoffi <matiasgoffi@hotmail.com>
2022-10-13 09:05:59 -0300

    cambio domains de next config

--- matias-nextjsapp/next.config.js
+++ matias-nextjsapp/next.config.js
@@ -5,18 +5,6 @@ const nextConfig = {
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: "en",
-    domains: [
-      {
-        domain: 'example.com',
-        defaultLocale: 'en-US',
-      },
-      {
-        domain: 'example.ar',
-        defaultLocale: 'es',
-      },
-    ],
-  
-  
   },
 
 }
